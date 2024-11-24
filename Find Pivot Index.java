15e77f0521aeea2a92e564092e159cd813a5c401
Avdhesh Upadhyay <142656855+Avdheshupadhyay@users.noreply.github.com>
2024-11-24 19:58:08 +0530

    Find Pivot Index.java

--- Find Pivot Index.java
+++ Find Pivot Index.java
@@ -1,7 +1,7 @@
 class Solution {
     public int pivotIndex(int[] nums) {
         int sum=0,ls=0;
-         for(int i:nums)  sum+=i;
+         for(int number:nums)  sum+=number;
 
          for(int i=0;i<nums.length;i++){
             if(ls*2==sum-nums[i]){
